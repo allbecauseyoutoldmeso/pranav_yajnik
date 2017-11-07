03baf42f8dfead182f4bede80c7a2936130c7f48
Kate Gleeson <allbecauseyoutoldmeso@yahoo.co.uk>
2017-11-07 21:41:15 +0000

    add photos.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -17,6 +17,12 @@ class PhotosController < ApplicationController
     end
   end
 
+  def destroy
+    photo = Photo.find(params[:id])
+    photo.destroy
+    redirect_to photos_path
+  end
+
   private
 
   def photo_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'home/index'   
+  get 'home/index'
   get '/about_pranav' => 'home#about_pranav'
   get '/gallery' => 'home#gallery'
   get '/lessons' => 'home#lessons'
