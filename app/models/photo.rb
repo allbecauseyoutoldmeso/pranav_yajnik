42433f0b8227028b7e5fdd690abf7ea310b78434
Kate Gleeson <allbecauseyoutoldmeso@yahoo.co.uk>
2017-11-12 16:13:58 +0000

    configure s3 bucket for images

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,8 @@
 class Photo < ActiveRecord::Base
   has_attached_file :image
   validates_attachment :image, content_type: { content_type: ['image/jpeg', 'image/gif', 'image/png'] }
+
+  def international_url
+    image.url.gsub('http://s3.amazonaws.com/pranav-photos', 'http://pranav-photos.s3.eu-west-2.amazonaws.com')
+  end
 end
