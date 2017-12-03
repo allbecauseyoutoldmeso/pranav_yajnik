d05109b9d9558d431463373cff07cbaa5ea46a93
Kate Gleeson <allbecauseyoutoldmeso@yahoo.co.uk>
2017-12-03 18:50:26 +0000

    config Rack::Deflater

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,8 @@ Bundler.require(*Rails.groups)
 
 module PranavYajnik
   class Application < Rails::Application
-
     config.load_defaults 5.1
-
     config.serve_static_assets = true
-
+    config.middleware.use Rack::Deflater
   end
 end
