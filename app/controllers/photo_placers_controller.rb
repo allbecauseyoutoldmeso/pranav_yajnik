7cec9fb1d21fc38004d989cbeab7743ef3da725c
Kate Gleeson <allbecauseyoutoldmeso@yahoo.co.uk>
2018-02-03 18:17:34 +0000

    enable admin to edit photo captions

--- app/controllers/photo_placers_controller.rb
+++ app/controllers/photo_placers_controller.rb
@@ -3,6 +3,8 @@ class PhotoPlacersController < ApplicationController
   def update
     photo_placer = PhotoPlacer.find(params[:id])
     photo_placer.update_attribute(:photo_id, params[:photo_id])
-    redirect_to photo_placer.url
+    @photo = Photo.find(params[:photo_id])
+    get_photo_placers
+    render 'photos/photo_frame'
   end
 end
